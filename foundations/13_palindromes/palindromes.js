da7515125ea75813488a00dad9e04081a485d10e
Usman <usman3996@gmail.com>
2026-04-11 13:32:59 +0500

    Complete 13_palindromes exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const processedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    const reversedString = processedString.split("").reverse().join("");
+    return processedString === reversedString;
 };
 
 // Do not edit below this line
